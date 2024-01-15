5b71aeded21f00baf0cb99eaf6b7ea7529ebf164
Bryan Richter <b@chreekat.net>
2024-01-15 11:33:50 +0200

    Repro with 2.0 as well

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ export AWS_ACCESS_KEY_ID=MyKeyId
 export AWS_SECRET_ACCESS_KEY=MySecretKey
 export AWS_S3_ENDPOINT=MyS2Endpoint
 
-stack run -- MyBucketName
+stack --stack-yaml stack.1.6.1.yaml run -- MyBucketName
 ```
 
 But you can also use standard ~/.aws config stuff.
--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE CPP #-}
 
 module Main where
 
+#if MIN_VERSION_amazonka(2,0,0)
+import Amazonka
+import Amazonka.S3
+import Amazonka.Env
+#else
 import Network.AWS
 import Network.AWS.S3
+#endif
 import System.Environment
 import System.IO
 import Control.Lens (set)
@@ -12,6 +19,20 @@ import qualified Data.Text as T
 import qualified Data.ByteString.Char8 as BS8
 
 run :: IO PutObjectResponse
+#if MIN_VERSION_amazonka(2,0,0)
+run = do
+    aws <- do
+        stdoutLog <- newLogger Debug stdout
+        aws'' <- newEnv discover
+        endpoint' <- lookup "AWS_S3_ENDPOINT" <$> getEnvironment
+        let aws' = set env_logger stdoutLog aws''
+        pure $ case endpoint' of
+            Nothing -> aws'
+            Just ep -> configureService (setEndpoint True (BS8.pack ep) 443 defaultService) aws'
+    [bkt] <- getArgs
+    let put = newPutObject (BucketName (T.pack bkt)) (ObjectKey "btest1") (toBody $ replicate 1048576 'A')
+    runResourceT $ send aws put
+#else
 run = do
     aws <- do
         stdoutLog <- newLogger Debug stdout
@@ -24,6 +45,7 @@ run = do
     [bkt] <- getArgs
     let put = putObject (BucketName (T.pack bkt)) (ObjectKey "btest1") "some content"
     runResourceT $ runAWS aws $ send put
+#endif
 
 main :: IO ()
 main = print =<< run
